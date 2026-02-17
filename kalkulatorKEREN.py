c96dfbb45e1008c9177a255bb2659decead7cf3b
arill2 <arilgo602@gmail.com>
2026-02-17 21:05:13 +0800

    first upload kalkulatorKEREN.py

---
+++ kalkulatorKEREN.py
@@ -0,0 +1,45 @@
+from tkinter import Tk, Entry, Button, StringVar
+
+class Calculator:
+    def __init__(self, master):
+        master.title("KALKULATOR BY ARIEL")
+        master.geometry("357x420+0+0")
+        master.config(bg="gray")
+        master.resizable(False, False)
+
+
+        self.equation=StringVar()
+        self.entry_value=''
+        Entry(width=17, bg='#fff', font=('Arial bold',28) , textvariable=self.equation).place(x=0,y=0)
+
+        Button(width=11, height=4 , text='(',relief='flat', bg='white', command=lambda:self.show('(')).place(x=0,y=50)
+        Button(width=11, height=4 , text=')',relief='flat', bg='white', command=lambda:self.show(')')).place(x=90,y=50)
+        Button(width=11, height=4 , text='%',relief='flat', bg='white', command=lambda:self.show('%')).place(x=180,y=50)
+        Button(width=11, height=4 , text='/',relief='flat', bg='white', command=lambda:self.show('/')).place(x=270,y=50)
+        Button(width=11, height=4 , text='7',relief='flat', bg='white', command=lambda:self.show('7')).place(x=0,y=150)
+        Button(width=11, height=4 , text='8',relief='flat', bg='white', command=lambda:self.show('8')).place(x=90,y=150)
+        Button(width=11, height=4 , text='9',relief='flat', bg='white', command=lambda:self.show('9')).place(x=180,y=150)
+        Button(width=11, height=4 , text='*',relief='flat', bg='white', command=lambda:self.show('*')).place(x=270,y=150)
+        Button(width=11, height=4 , text='4',relief='flat', bg='white', command=lambda:self.show('4')).place(x=0,y=250)
+        Button(width=11, height=4 , text='5',relief='flat', bg='white', command=lambda:self.show('5')).place(x=90,y=250)
+        Button(width=11, height=4 , text='6',relief='flat', bg='white', command=lambda:self.show('6')).place(x=180,y=250)
+        Button(width=11, height=4 , text='+',relief='flat', bg='white', command=lambda:self.show('+')).place(x=270,y=250)
+        Button(width =  33,height =  4,text = 'C' , relief = 'flat' ,bg = 'lightblue' ,command = self.clear).place(x =  93,y = 350)
+        Button(width =  11,height =  4,text = '=' , relief = 'flat' ,bg = 'lightblue' ,command = self.solve).place(x = 270,y = 350)
+        
+    def show(self,value):
+        self.entry_value+=str(value)
+        self.equation.set(self.entry_value)
+
+    def clear(self):
+        self.entry_value=''
+        self.equation.set(self.entry_value)
+
+    def solve(self):
+        result=eval(self.entry_value)
+        self.equation.set(result)
+
+
+root=Tk()
+calculator=Calculator(root)
+root.mainloop()
